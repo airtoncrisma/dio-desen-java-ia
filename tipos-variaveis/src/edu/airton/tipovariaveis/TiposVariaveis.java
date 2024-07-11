cb75ed808807951809b245d471c086facc520989
airtoncrisma <atopomap@outlook.com>
2024-07-11 11:59:13 -0300

    Organizar package

--- tipos-variaveis/src/edu/airton/tipovariaveis/TiposVariaveis.java
+++ tipos-variaveis/src/edu/airton/tipovariaveis/TiposVariaveis.java
@@ -1,6 +1,7 @@
+package edu.airton.tipovariaveis;
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
         
     }
 }
